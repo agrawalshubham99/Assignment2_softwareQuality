1a3e58109f122eef9015fd779c74046a567fac60
Shubham Agrawal <sagrawal6665@conestogac.on.ca>
2024-06-03 20:42:07 -0400

    added files

---
+++ Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Group1_Shubham_Anila_Assignment2
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public int Price { get; set; }
+        public int Stock { get; set; }
+
+        public Product(int ProductID, string ProductName, int Price, int Stock)
+        {
+            this.ProductID = ProductID;
+            this.ProductName = ProductName;
+            this.Price = Price;
+            this.Stock = Stock;
+        }
+
+
+        public void Increase(int count){ Stock = Stock + count; }
+
+        public void Decrease(int count) { Stock = Stock - count; }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Group1_Shubham_Anila_Assignment2;
+
+namespace Group1_Shubham_Anila_Assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
